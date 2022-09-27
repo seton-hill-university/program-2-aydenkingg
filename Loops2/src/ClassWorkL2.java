2e50d5842ee9fa81781f93a628b1766ac890b6c2
aydenking <aydenking@10.237.3.165>
2022-09-27 10:55:01 -0400

    thanks

---
+++ Loops2/src/ClassWorkL2.java
@@ -0,0 +1,24 @@
+//methods, voids, loops
+// Application:	Prog 1
+//Name: Ayden King
+//GitHub User: Aydenkingg
+//version 1.0
+// Teacher: Brad Messner
+public class ClassWorkL2 {
+	public static void main(String[] args) {
+		//Auto generated method stub
+		
+		displayMessage();
+		displayMessage2();
+
+	}
+	public static void displayMessage()
+	{
+	System.out.println("Hello");
+	}
+	public static void displayMessage2()
+	{
+	System.out.println("Hello Again");
+	}
+	
+}
